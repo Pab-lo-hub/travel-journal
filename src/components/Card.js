2fdf1c1ca524f2e4066f52139c0af52c13621d34
Pab-lo <salaberry.pablo@gmail.com>
2022-07-25 23:26:34 +0200

    Location icon added

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,9 @@ export default function Card(props) {
         <div className="card">
             <img src={`${props.item.imageUrl}`} className="card--image" />
             <div className="card--body">
-            <p className="card--location">{props.item.location} <a href="{props.item.googleMapsUrl}" className="card--link">View on Google Maps</a></p>
+            <p className="card--location"><img src="../images/path.png" className="path-icon"/>  {props.item.location} <a href="{props.item.googleMapsUrl}" className="card--link">View on Google Maps</a></p>
             <h1 className="card--title">{props.item.title}</h1>
-            <h5 className="card--date">{props.item.startDate} {props.item.endDate}</h5>
+            <h5 className="card--date">{props.item.startDate} - {props.item.endDate}</h5>
             <p className="card--text">{props.item.description}</p>
             </div>
         </div>
